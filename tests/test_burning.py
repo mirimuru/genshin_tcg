47f4a82ac74e540dc6976ae577f47a54985bf1cb
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-15 20:51:23 +0900

    test: add burning reaction behavior tests

---
+++ tests/test_burning.py
@@ -0,0 +1,80 @@
+import pytest
+
+from engine.actions import Action, ActionType
+from engine.dice import DicePool
+from engine.game import Game
+from engine.state import CharacterState, Element, GameState, PlayerState
+
+
+def make_game():
+    players = []
+    for player_id in (0, 1):
+        characters = [
+            CharacterState("キャラクター1", Element.PYRO),
+            CharacterState("キャラクター2", Element.HYDRO),
+            CharacterState("キャラクター3", Element.CRYO),
+        ]
+        players.append(PlayerState(player_id, characters))
+
+    game = Game(GameState(players))
+    game.execute_action(Action(0, ActionType.REROLL_DICE, target=()))
+    game.execute_action(Action(1, ActionType.REROLL_DICE, target=()))
+    game.state.players[0].dice = DicePool.default()
+    game.state.players[1].dice = DicePool.default()
+    return game
+
+
+def test_burning_reaction_applies_burning_status():
+    game = make_game()
+    target = game.state.players[1].active_character
+    target.elemental_aura = Element.DENDRO
+
+    game.deal_damage(0, 1, 2, Element.PYRO)
+
+    assert "burning" in target.statuses
+    assert target.hp == 7
+    assert target.elemental_aura is None
+
+
+def test_burning_status_deals_one_damage_at_end_of_round_and_is_removed():
+    game = make_game()
+    target = game.state.players[1].active_character
+    target.elemental_aura = Element.DENDRO
+    game.deal_damage(0, 1, 2, Element.PYRO)
+    hp_before = target.hp
+
+    game.execute_action(Action(0, ActionType.END_ROUND))
+    game.execute_action(Action(1, ActionType.END_ROUND))
+
+    assert hp_before - target.hp == 1
+    assert "burning" not in target.statuses
+
+
+def test_burning_damage_can_end_the_game():
+    game = make_game()
+    target_player = game.state.players[1]
+    target = target_player.active_character
+    target.elemental_aura = Element.DENDRO
+    target.receive_damage(8)
+
+    game.deal_damage(0, 1, 1, Element.PYRO)
+    assert "burning" in target.statuses
+    assert target.hp == 0
+
+    for character in target_player.characters[1:]:
+        character.receive_damage(999)
+
+    game.state.check_game_over()
+    assert game.state.game_over
+
+
+def test_reapplying_burning_does_not_duplicate_status():
+    game = make_game()
+    target = game.state.players[1].active_character
+    target.elemental_aura = Element.DENDRO
+    game.deal_damage(0, 1, 1, Element.PYRO)
+
+    target.elemental_aura = Element.DENDRO
+    game.deal_damage(0, 1, 1, Element.PYRO)
+
+    assert target.statuses.count("burning") == 1
