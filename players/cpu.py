58499828b92022dfff930c29309af4e96fbe9901
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-15 13:29:02 +0900

    feat: make CPU choose from legal actions

--- players/cpu.py
+++ players/cpu.py
@@ -2,70 +2,49 @@ from engine.actions import Action, ActionType
 
 
 class CpuPlayer:
-    """現在のゲーム状態から単一の行動を選択するCPUプレイヤー。
-
-    現段階では、ゲームエンジン側のダイス消費やカード処理が未実装でも
-    動作できるよう、状態だけから判断できるヒューリスティックを使用する。
-    """
+    """合法手一覧から行動を選択するヒューリスティックCPU。"""
 
     LOW_HP_THRESHOLD = 3
 
-    def choose_action(self, game, player_id: int) -> Action:
-        """現在の状態からCPUが実行したい行動を返す。"""
+    def choose_action(self, game, player_id: int, legal_actions=None) -> Action:
+        if legal_actions is None:
+            legal_actions = game.get_legal_actions(player_id)
+        if not legal_actions:
+            return Action(player_id, ActionType.END_ROUND)
+
+        def find(action_type):
+            return next((action for action in legal_actions if action.action_type is action_type), None)
+
+        switch_actions = [
+            action for action in legal_actions
+            if action.action_type is ActionType.SWITCH_CHARACTER
+        ]
         player = game.state.players[player_id]
 
-        # アクティブキャラクターが倒れている場合は、まず強制交代を行う。
-        if player.requires_switch:
-            return Action(
-                player_id=player_id,
-                action_type=ActionType.SWITCH_CHARACTER,
-                target=self._best_switch_target(player),
-            )
+        if player.requires_switch and switch_actions:
+            return self._best_switch_action(player, switch_actions)
 
-        # 全員戦闘不能ならゲーム終了済みなので行動は不要。
-        if player.defeated:
-            return Action(player_id=player_id, action_type=ActionType.END_ROUND)
+        burst = find(ActionType.ELEMENTAL_BURST)
+        if burst is not None:
+            return burst
 
-        # 元素爆発を使用できるなら最優先する。
-        character = player.active_character
-        if character.energy >= character.max_energy:
-            return Action(
-                player_id=player_id,
-                action_type=ActionType.ELEMENTAL_BURST,
-            )
+        if player.active_character.hp <= self.LOW_HP_THRESHOLD and switch_actions:
+            return self._best_switch_action(player, switch_actions)
 
-        # HPが危険域なら、残っているキャラクターへ交代する。
-        if character.hp <= self.LOW_HP_THRESHOLD:
-            target = self._best_switch_target(player)
-            if target is not None:
-                return Action(
-                    player_id=player_id,
-                    action_type=ActionType.SWITCH_CHARACTER,
-                    target=target,
-                )
-
-        # 次点として元素スキルを選択する。
-        return Action(
-            player_id=player_id,
-            action_type=ActionType.ELEMENTAL_SKILL,
-        )
+        skill = find(ActionType.ELEMENTAL_SKILL)
+        if skill is not None:
+            return skill
+        attack = find(ActionType.NORMAL_ATTACK)
+        if attack is not None:
+            return attack
+        return find(ActionType.END_ROUND) or legal_actions[0]
 
     @staticmethod
-    def _best_switch_target(player):
-        """交代先として最もHP割合の高い生存キャラクターを選ぶ。"""
-        candidates = [
-            index
-            for index in player.alive_character_indices()
-            if index != player.active_character_index
-        ]
-
-        if not candidates:
-            return None
-
+    def _best_switch_action(player, actions):
         return max(
-            candidates,
-            key=lambda index: (
-                player.characters[index].hp / player.characters[index].max_hp,
-                player.characters[index].hp,
+            actions,
+            key=lambda action: (
+                player.characters[action.target].hp / player.characters[action.target].max_hp,
+                player.characters[action.target].hp,
             ),
         )
