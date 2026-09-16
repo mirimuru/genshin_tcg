8ba1b01dc944808344994a22c275a3c6ecc3fe2f
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-17 00:07:20 +0900

    feat: add Diluc character definition

--- content/characters/diluc.py
+++ content/characters/diluc.py
@@ -3,31 +3,22 @@ from engine.state import Element
 
 
 class Diluc(CharacterDefinition):
+    """ディルックの基本Definition。"""
+
+    character_id = "diluc"
     name = "ディルック"
     element = Element.PYRO
     max_hp = 10
     max_energy = 2
 
-    def normal_attack(self, game, player_id):
-        game.deal_damage(
-            attacker_id=player_id,
-            target_id=1 - player_id,
-            amount=2,
-            element=Element.PHYSICAL,
-        )
+    def normal_attack(self, game, player_id: int) -> None:
+        game.deal_damage(player_id, 1 - player_id, 2, Element.PHYSICAL)
 
-    def elemental_skill(self, game, player_id):
-        game.deal_damage(
-            attacker_id=player_id,
-            target_id=1 - player_id,
-            amount=3,
-            element=Element.PYRO,
-        )
+    def elemental_skill(self, game, player_id: int) -> None:
+        game.deal_damage(player_id, 1 - player_id, 3, Element.PYRO)
 
-    def elemental_burst(self, game, player_id):
-        game.deal_damage(
-            attacker_id=player_id,
-            target_id=1 - player_id,
-            amount=8,
-            element=Element.PYRO,
-        )
+    def elemental_burst(self, game, player_id: int) -> None:
+        game.deal_damage(player_id, 1 - player_id, 4, Element.PYRO)
+
+
+DILUC = Diluc()
