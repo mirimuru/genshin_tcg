b2ec3dddc0220f487a730742c450981c9a14c8dc
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-17 01:28:24 +0900

    feat: register Kaeya character definition

--- content/characters/__init__.py
+++ content/characters/__init__.py
@@ -1,4 +1,5 @@
 from content.characters.diluc import DILUC, Diluc
+from content.characters.kaeya import KAEYA, Kaeya
 from content.characters.xiangling import XIANG_LING, Xiangling
 
-__all__ = ["DILUC", "Diluc", "XIANG_LING", "Xiangling"]
+__all__ = ["DILUC", "Diluc", "KAEYA", "Kaeya", "XIANG_LING", "Xiangling"]
