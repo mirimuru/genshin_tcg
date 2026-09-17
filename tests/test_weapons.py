20c2d0601cfadb222712a4331f965c75e05d6278
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-17 21:58:07 +0900

    test: cover concrete weapon card behavior

--- tests/test_weapons.py
+++ tests/test_weapons.py
@@ -4,11 +4,8 @@ from engine.cards import WeaponCardDefinition
 from engine.dice import DicePool, DiceType
 from engine.game import Game
 from engine.state import CharacterState, Element, GamePhase, GameState, PlayerState
-from engine.statuses import StatusInstance, WeaponEquipmentStatusDefinition
-
-
-class TestSwordCharacter:
-    pass
+from engine.statuses import EquipmentStatusDefinition, StatusInstance, WeaponEquipmentStatusDefinition
+from content.cards.weapons import TRAVELERS_HANDY_SWORD, TravelersHandySwordStatus
 
 
 def make_game():
@@ -25,12 +22,7 @@ def make_game():
 
 
 def test_weapon_equipment_status_uses_weapon_slot_and_type():
-    class SwordStatus(WeaponEquipmentStatusDefinition):
-        status_id = "test_sword"
-        name = "テスト片手剣"
-        weapon_type = "sword"
-
-    status = StatusInstance(SwordStatus)
+    status = StatusInstance(TravelersHandySwordStatus)
 
     assert status.definition.equipment_slot == "weapon"
     assert status.definition.weapon_type == "sword"
@@ -46,27 +38,13 @@ def test_character_definition_exposes_weapon_type():
 
 
 def test_weapon_card_definition_requires_matching_weapon_type():
-    class SwordStatus(WeaponEquipmentStatusDefinition):
-        status_id = "test_sword"
-        name = "テスト片手剣"
-        weapon_type = "sword"
-
-    class SwordCard(WeaponCardDefinition):
-        card_id = "test_sword_card"
-        name = "テスト片手剣"
-        cost = {DiceType.ANY: 2}
-        weapon_type = "sword"
-
-        def create_status(self):
-            return StatusInstance(SwordStatus)
-
     game = make_game()
     game.state.players[0].active_character.definition.weapon_type = "sword"
 
-    assert SwordCard().can_play(game, 0)
+    assert TRAVELERS_HANDY_SWORD.can_play(game, 0)
 
     game.state.players[0].active_character.definition.weapon_type = "bow"
-    assert not SwordCard().can_play(game, 0)
+    assert not TRAVELERS_HANDY_SWORD.can_play(game, 0)
 
 
 def test_equipping_weapon_replaces_previous_weapon_but_keeps_talent():
@@ -79,8 +57,6 @@ def test_equipping_weapon_replaces_previous_weapon_but_keeps_talent():
         status_id = "sword_b"
         name = "剣B"
         weapon_type = "sword"
-
-    from engine.statuses import EquipmentStatusDefinition
 
     class TalentStatus(EquipmentStatusDefinition):
         status_id = "talent"
@@ -97,28 +73,10 @@ def test_equipping_weapon_replaces_previous_weapon_but_keeps_talent():
     assert character.has_status("sword_b")
 
 
-def test_weapon_increases_only_normal_attack_damage():
-    class SwordStatus(WeaponEquipmentStatusDefinition):
-        status_id = "damage_sword"
-        name = "攻撃用片手剣"
-        weapon_type = "sword"
-
-        def on_event(self, instance, event, game, context):
-            from engine.events import NormalAttackEvent
-
-            if isinstance(event, NormalAttackEvent) and event.player_id == context.owner_id and not event.resolved:
-                instance.data["armed"] = True
-
-        def modify_damage(self, instance, amount, element, game, context):
-            if instance.data.pop("armed", False):
-                return amount + 1, element
-            return amount, element
-
-    character = make_game().state.players[0].active_character
-    character.definition.weapon_type = "sword"
-    character.add_equipment(StatusInstance(SwordStatus))
+def test_travelers_handy_sword_increases_only_normal_attack_damage():
     game = make_game()
-    game.state.players[0].characters[0] = character
+    game.state.players[0].active_character.definition.weapon_type = "sword"
+    game.state.players[0].active_character.add_equipment(StatusInstance(TravelersHandySwordStatus))
 
     game.normal_attack(0)
     assert game.state.players[1].active_character.hp == 7
@@ -128,10 +86,13 @@ def test_weapon_increases_only_normal_attack_damage():
 
 
 def test_weapon_card_rejects_invalid_weapon_type():
+    class InvalidWeaponCard(WeaponCardDefinition):
+        card_id = "invalid_weapon"
+        name = "不正武器"
+        weapon_type = "boomerang"
+
+        def create_status(self):
+            return None
+
     with pytest.raises(ValueError, match="weapon_type"):
-        type("InvalidWeaponCard", (WeaponCardDefinition,), {
-            "card_id": "invalid_weapon",
-            "name": "不正武器",
-            "weapon_type": "boomerang",
-            "create_status": lambda self: None,
-        })()
+        InvalidWeaponCard()
