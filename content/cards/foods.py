1387826fb904e2638a6e21b37ca93cd9177f5988
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-17 17:43:31 +0900

    feat: add Jueyun Guoba card status

--- content/cards/foods.py
+++ content/cards/foods.py
@@ -1,5 +1,7 @@
 from engine.cards import CardDefinition
 from engine.dice import DiceType
+from engine.events import NormalAttackEvent
+from engine.statuses import StatusDefinition, StatusInstance
 
 
 class SweetMadame(CardDefinition):
@@ -19,4 +21,34 @@ class SweetMadame(CardDefinition):
         character.heal(1)
 
 
+class JueyunGuobaStatus(StatusDefinition):
+    """次に使用する通常攻撃のダメージを1増加させる。"""
+
+    status_id = "jueyun_guoba"
+    name = "絶雲お焦げ"
+    max_usages = 1
+
+    def on_event(self, instance, event, game, context):
+        if isinstance(event, NormalAttackEvent) and event.player_id == context.player_id and not event.resolved:
+            instance.data["armed"] = True
+
+    def modify_damage(self, instance, amount, element, game, context):
+        if instance.data.pop("armed", False):
+            instance.consume()
+            return amount + 1, element
+        return amount, element
+
+
+class JueyunGuoba(CardDefinition):
+    """絶雲お焦げ。次の通常攻撃のダメージを1増加させる。"""
+
+    card_id = "jueyun_guoba"
+    name = "絶雲お焦げ"
+    cost = {DiceType.ANY: 1}
+
+    def play(self, game, player_id, target=None):
+        game.state.players[player_id].add_combat_status(StatusInstance(JueyunGuobaStatus))
+
+
 SWEET_MADAME = SweetMadame()
+JUEYUN_GUOBA = JueyunGuoba()
