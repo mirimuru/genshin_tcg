edb201e06df0913a73c3bd74681b02b060c50cc4
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-19 15:16:08 +0900

    test: add sacrificial weapon family coverage

--- tests/test_sacrificial_weapon.py
+++ tests/test_sacrificial_weapon.py
@@ -3,7 +3,12 @@ from engine.characters import CharacterDefinition as PublicCharacterDefinition
 from engine.dice import DicePool, DiceType
 from engine.game import Game
 from engine.state import CharacterState, Element, GamePhase, GameState, PlayerState
-from content.cards.weapons import SacrificialSword
+from content.cards.weapons import (
+    SacrificialBow,
+    SacrificialFragments,
+    SacrificialGreatsword,
+    SacrificialSword,
+)
 
 
 class SwordCharacter(PublicCharacterDefinition):
@@ -11,62 +16,99 @@ class SwordCharacter(PublicCharacterDefinition):
     name = "テスト片手剣"
     element = Element.PYRO
     weapon_type = "sword"
-
     normal_attack_cost = {DiceType.PYRO: 3}
     elemental_skill_cost = {DiceType.PYRO: 3}
 
 
+class GreatswordCharacter(PublicCharacterDefinition):
+    character_id = "test_sacrificial_greatsword"
+    name = "テスト両手剣"
+    element = Element.PYRO
+    weapon_type = "claymore"
+    normal_attack_cost = {DiceType.PYRO: 3}
+    elemental_skill_cost = {DiceType.PYRO: 3}
+
+
+class BowCharacter(PublicCharacterDefinition):
+    character_id = "test_sacrificial_bow"
+    name = "テスト弓"
+    element = Element.CRYO
+    weapon_type = "bow"
+    normal_attack_cost = {DiceType.CRYO: 3}
+    elemental_skill_cost = {DiceType.CRYO: 3}
+
+
+class CatalystCharacter(PublicCharacterDefinition):
+    character_id = "test_sacrificial_fragments"
+    name = "テスト法器"
+    element = Element.ELECTRO
+    weapon_type = "catalyst"
+    normal_attack_cost = {DiceType.ELECTRO: 3}
+    elemental_skill_cost = {DiceType.ELECTRO: 3}
+
+
 SWORD_CHARACTER = SwordCharacter()
+GREATSWORD_CHARACTER = GreatswordCharacter()
+BOW_CHARACTER = BowCharacter()
+CATALYST_CHARACTER = CatalystCharacter()
 
 
-def make_game():
+def make_game(character_definition):
     players = [
-        PlayerState(0, [SWORD_CHARACTER.create_state(), CharacterState("B", Element.HYDRO), CharacterState("C", Element.CRYO)]),
+        PlayerState(0, [character_definition.create_state(), CharacterState("B", Element.HYDRO), CharacterState("C", Element.CRYO)]),
         PlayerState(1, [CharacterState("X", Element.PYRO), CharacterState("Y", Element.HYDRO), CharacterState("Z", Element.GEO)]),
     ]
     game = Game(GameState(players))
     game.state.phase = GamePhase.ACTION
     game.state.current_player = 0
-    players[0].dice = DicePool({DiceType.PYRO: 8})
+    players[0].dice = DicePool({DiceType.OMNI: 8})
     players[1].dice = DicePool({DiceType.OMNI: 8})
     return game
 
 
-def test_sacrificial_sword_has_matching_weapon_type_and_cost():
-    card = SacrificialSword()
-    game = make_game()
-    game.card_registry.register(card)
-    game.state.players[0].dice = DicePool({DiceType.PYRO: 3})
-
-    assert card.can_play(game, 0)
-    assert game.get_action_cost(Action(0, ActionType.PLAY_CARD, card_id=card.card_id)) == {DiceType.PYRO: 3}
-
-
-def test_sacrificial_sword_reduces_next_skill_cost_after_skill():
-    card = SacrificialSword()
-    game = make_game()
-    game.state.players[0].active_character.add_equipment(card.create_status())
-
-    skill = Action(0, ActionType.ELEMENTAL_SKILL)
-    attack = Action(0, ActionType.NORMAL_ATTACK)
-
-    assert game.get_action_cost(skill) == {DiceType.PYRO: 3}
-    game.elemental_skill(0)
-
-    assert game.get_action_cost(attack) == {DiceType.PYRO: 2}
-    assert game.get_action_cost(attack) == {DiceType.PYRO: 2}
-
-    game.normal_attack(0)
-    assert game.get_action_cost(attack) == {DiceType.PYRO: 3}
+def test_sacrificial_weapon_cards_match_weapon_type_and_cost():
+    cases = [
+        (SacrificialSword(), SWORD_CHARACTER, DiceType.PYRO),
+        (SacrificialGreatsword(), GREATSWORD_CHARACTER, DiceType.PYRO),
+        (SacrificialBow(), BOW_CHARACTER, DiceType.CRYO),
+        (SacrificialFragments(), CATALYST_CHARACTER, DiceType.ELECTRO),
+    ]
+    for card, character, dice_type in cases:
+        game = make_game(character)
+        game.state.players[0].dice = DicePool({dice_type: 3})
+        game.card_registry.register(card)
+        action = Action(0, ActionType.PLAY_CARD, card_id=card.card_id)
+        assert card.can_play(game, 0)
+        assert game.get_action_cost(action) == {dice_type: 3}
 
 
-def test_sacrificial_sword_does_not_apply_before_skill_or_to_other_character():
-    card = SacrificialSword()
-    game = make_game()
-    game.state.players[0].active_character.add_equipment(card.create_status())
+def test_sacrificial_weapon_cards_reject_wrong_weapon_type():
+    cases = [
+        (SacrificialSword(), GreatswordCharacter()),
+        (SacrificialGreatsword(), SwordCharacter()),
+        (SacrificialBow(), CatalystCharacter()),
+        (SacrificialFragments(), BowCharacter()),
+    ]
+    for card, wrong_character in cases:
+        game = make_game(wrong_character)
+        game.state.players[0].dice = DicePool({DiceType.PYRO: 3, DiceType.CRYO: 3, DiceType.ELECTRO: 3})
+        assert card.can_play(game, 0) is False
 
-    attack = Action(0, ActionType.NORMAL_ATTACK)
-    assert game.get_action_cost(attack) == {DiceType.PYRO: 3}
 
-    game.state.players[0].active_character_index = 1
-    assert game.get_action_cost(attack) == {DiceType.HYDRO: 3}
+def test_sacrificial_weapon_reduces_next_attack_action_cost_after_skill():
+    cases = [
+        (SacrificialSword(), SWORD_CHARACTER, DiceType.PYRO),
+        (SacrificialGreatsword(), GREATSWORD_CHARACTER, DiceType.PYRO),
+        (SacrificialBow(), BOW_CHARACTER, DiceType.CRYO),
+        (SacrificialFragments(), CATALYST_CHARACTER, DiceType.ELECTRO),
+    ]
+    for card, character, dice_type in cases:
+        game = make_game(character)
+        game.state.players[0].active_character.add_equipment(card.create_status())
+        skill = Action(0, ActionType.ELEMENTAL_SKILL)
+        attack = Action(0, ActionType.NORMAL_ATTACK)
+        assert game.get_action_cost(skill) == {dice_type: 3}
+        game.elemental_skill(0)
+        assert game.get_action_cost(attack) == {dice_type: 2}
+        game.normal_attack(0)
+        assert game.get_action_cost(attack) == {dice_type: 3}
