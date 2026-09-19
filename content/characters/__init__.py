54fcd3937636a71b4d2d40377ee0ef8da6c38463
mirimuru <142558738+mirimuru@users.noreply.github.com>
2026-09-19 15:27:14 +0900

    feat: export Xingqiu character

--- content/characters/__init__.py
+++ content/characters/__init__.py
@@ -1,5 +1,6 @@
 from content.characters.diluc import DILUC, Diluc
 from content.characters.kaeya import KAEYA, Kaeya
 from content.characters.xiangling import XIANG_LING, Xiangling
+from content.characters.xingqiu import XINGQIU, Xingqiu
 
-__all__ = ["DILUC", "Diluc", "KAEYA", "Kaeya", "XIANG_LING", "Xiangling"]
+__all__ = ["DILUC", "Diluc", "KAEYA", "Kaeya", "XIANG_LING", "Xiangling", "XINGQIU", "Xingqiu"]
